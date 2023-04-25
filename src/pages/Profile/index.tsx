03a79a5a3f5ada9aeaed1b8bbcdc900886c50136
M.O.K.K.A <marbeloz.studio@gmail.com>
2023-04-25 22:12:32 +0200

    user is redirected to the login page if token is null while on profile page

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,19 @@
 //Imports
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { selectToken } from '../../features/auth/authSlice';
+import { useNavigate } from 'react-router-dom';
 
 //Component of the profile page
 export default function Profile() {
+    const redirect = useNavigate();
+    const token = useSelector(selectToken);
+
+    useEffect(() => {
+        if (token === null) {
+            redirect('/login', { replace: true });
+        }
+    }, [redirect, token]);
+
     return <main></main>;
 }
